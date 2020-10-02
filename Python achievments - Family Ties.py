4b25ad1c0a96d0006371a7759af5a73ebb78ef37
Milan de Ridder <30842@ma-web.nl>
2020-10-02 11:46:42 +0200

    Added Family Ties

---
+++ Python achievments - Family Ties.py
@@ -0,0 +1,13 @@
+#variabelen
+naam = str("Mijn naam is Milan de Ridder. ")
+leeftijd = str("ik ben 16 jaar oud en ben jarig op 27 april. ")
+woonplaats = str("Ik woon op het adres Krommenie Kastanjestraat 10. ")
+afstand = str("ik woon ongeveer 20km van het Mediacollege. ")
+andere_Info = str("Ik vind het leuk om te gamen en om anime te kijken") 
+
+#codes
+print(naam)
+print(leeftijd)
+print(woonplaats)
+print(afstand)
+print(andere_Info)
