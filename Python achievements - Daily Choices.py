e19a7df266e1ad6374737b2a1fa9f160afed4fc6
Milan de Ridder <30842@ma-web.nl>
2020-10-02 12:50:24 +0200

    Daily Choices en Family Ties toegevoegd

---
+++ Python achievements - Daily Choices.py
@@ -0,0 +1,55 @@
+print("Hier komen 5 keuze vragen")
+print("je moet je antwoorden altijd beantwoorden met (HOOFDLETTERS)")
+print("--------------------------------------------------------")
+print("Eet je liever GEZOND of ONGEZOND eten?")
+keuze = input()
+if keuze == 'GEZOND':
+    print("Je eet liever gezond")
+elif keuze == 'ONGEZOND':
+    print("Je eet liever ongezond")
+else:
+    print("Je keuze is niet beschikbaar")
+
+print("--------------------------------------------------------")
+print("Ben je liever aan het GAMEN of ben je liever BUITEN?")
+keuze = input()
+if keuze == 'GAMEN':
+    print("Je bent liever aan het gamen")
+elif keuze == 'BUITEN':
+    print("Je bent liever buiten")
+else:
+    print("Je keuze is niet beschikbaar")
+
+print("--------------------------------------------------------")
+print("drink je liever WARME DRANKEN of KOUDE DRANKEN?")
+keuze = input()
+if keuze == 'WARME DRANKEN':
+    print("je drinkt liever warme dranken")
+elif keuze == 'KOUDE DRANKEN':
+    print("je drinkt liever koude dranken")
+else:
+    print("Je keuze is niet beschikbaar")
+
+print("--------------------------------------------------------")
+print("Vind je anime LEUK of NIET LEUK?")
+keuze = input()
+if keuze == 'LEUK':
+    print("Je vind anime leuk")
+elif keuze == 'NIET LEUK':
+    print("Je vind anime niet leuk")
+else:
+     print("Je keuze is niet beschikbaar")
+
+print("--------------------------------------------------------")
+print("vond je deze vragen LEUK of NIET LEUK?")
+keuze = input()
+if keuze == 'LEUK':
+    print("Dat is fijn om te horen")
+elif keuze == 'NIET LEUK':
+    print("dat is jammer")
+else:
+    print("Je keuze is niet beschikbaar")
+
+print("--------------------------------------------------------")
+print("dit was het einde") 
+
--- Python achievments - Family Ties.py
+++ Python achievments - Family Ties.py
@@ -1,13 +1,10 @@
 #variabelen
-naam = str("Mijn naam is Milan de Ridder. ")
-leeftijd = str("ik ben 16 jaar oud en ben jarig op 27 april. ")
-woonplaats = str("Ik woon op het adres Krommenie Kastanjestraat 10. ")
-afstand = str("ik woon ongeveer 20km van het Mediacollege. ")
-andere_Info = str("Ik vind het leuk om te gamen en om anime te kijken") 
+naam = ("Milan de Ridder")
+leeftijd = ("16 jaar")
+woonplaats = ("Krommenie Kastanjastraat 10")
+afstand = ("ongeveer 20km")
+andere_Info = ("Ik vind het leuk om te gamen en om anime te kijken")
 
 #codes
-print(naam)
-print(leeftijd)
-print(woonplaats)
-print(afstand)
-print(andere_Info)
+print("Mijn naam is " +  naam  + "ik ben " + leeftijd + "oud en ik woon op het adres " + woonplaats + "Het Mediacollege is " + afstand + " van mijn huis vandaan. en " + andere_Info)
+
