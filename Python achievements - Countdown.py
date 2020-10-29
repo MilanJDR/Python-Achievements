de7f447cb03b89e943f3dbd80af0c1685e3f894b
Milan de Ridder <30842@ma-web.nl>
2020-10-29 12:11:37 +0100

    Countdown toegevoegd

---
+++ Python achievements - Countdown.py
@@ -0,0 +1,9 @@
+countdown = 10000
+
+while countdown > 0:
+    print (countdown)
+    countdown = countdown - 1
+    if countdown == 0:
+        print("https://www.youtube.com/watch?v=oHg5SJYRHA0")
+     
+       
