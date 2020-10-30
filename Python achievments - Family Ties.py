753aa498dd30ecc4426738276f363b7e5bfdccf4
Milan de Ridder <30842@ma-web.nl>
2020-10-30 12:46:21 +0100

    Family Ties verbeterd

--- Python achievments - Family Ties.py
+++ Python achievments - Family Ties.py
@@ -1,10 +1,10 @@
 #variabelen
 naam = ("Milan de Ridder")
-leeftijd = ("16 jaar")
+leeftijd = 16  
 woonplaats = ("Krommenie Kastanjastraat 10")
-afstand = ("ongeveer 20km")
+afstand = 20
 andere_Info = ("Ik vind het leuk om te gamen en om anime te kijken")
 
 #codes
-print("Mijn naam is " +  naam  + "ik ben " + leeftijd + "oud en ik woon op het adres " + woonplaats + "Het Mediacollege is " + afstand + " van mijn huis vandaan. en " + andere_Info)
+print("Mijn naam is " +  naam  + " ik ben " + str(leeftijd) + " jaar oud en ik woon op het adres " + woonplaats + " Het Mediacollege is ongeveer " + str(afstand) + " km van mijn huis vandaan. en " + andere_Info)
 
